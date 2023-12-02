345519685413453309406f7e584c7af6498487c0
Orhan Kürşad YAVUZLU <yavuzluorhankursad@gmail.com>
2023-12-02 13:10:56 +0300

    -Added update product and validation

--- ProductInventoryMvc/Controllers/ProductController.cs
+++ ProductInventoryMvc/Controllers/ProductController.cs
@@ -42,8 +42,37 @@ namespace ProductInventoryMvc.Controllers
 
         public ActionResult Delete(int id)
         {
-            var ctg = db.Tbl_Products.Find(id);
-            db.Tbl_Products.Remove(ctg);
+            var prd = db.Tbl_Products.Find(id);
+            db.Tbl_Products.Remove(prd);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult GetProduct(int id)
+        {
+            var prd = db.Tbl_Products.Find(id);
+
+            List<SelectListItem> values = (from i in db.Tbl_Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.CategoryName,
+                                               Value = i.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.vl = values;
+
+            return View("GetProduct", prd);
+        }
+
+        public ActionResult Update(Tbl_Products p)
+        {
+            var prd = db.Tbl_Products.Find(p.ProductID);
+            prd.ProductName = p.ProductName;
+            prd.ProductTrademark = p.ProductTrademark;
+            prd.ProductStock = p.ProductStock;
+            prd.ProductPrice = p.ProductPrice;
+            //prd.ProductCategory = p.ProductCategory;
+            var ctg = db.Tbl_Categories.Where(m => m.CategoryID == p.Tbl_Categories.CategoryID).FirstOrDefault();
+            prd.ProductCategory = ctg.CategoryID;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
